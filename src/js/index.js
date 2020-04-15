6c197685d8063b270f5c7e564742f102f35efeb6
NadineDon <Nadine.Donie@stud.uni-regensburg.de>
2020-04-15 14:05:14 +0200

    get data

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,39 @@
+
+    getData();
+     function getData() { //for first method: add async before function
+        //option1
+        /*
+        const responseDeathsGlobalsCovid19 = await fetch("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv");
+        const deathsGlobalCovid19 = await responseDeathsGlobalsCovid19.text();
+        const responseInfectedGlobalsCovid19 = await fetch("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv");
+        const infectedGlobalCovid19 = await responseInfectedGlobalsCovid19.text();
+        const responseRecoveredCovid19 = await fetch("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
+        const recoveredCovid19 = await responseRecoveredCovid19.text;
+         */
+
+        //option2 to get csv data
+        d3.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv", 
+        function(deathsGlobalCovid19) {
+            console.log(deathsGlobalCovid19)
+        });
+
+        d3.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv", 
+        function(infectedGlobalCovid19) {
+            console.log(infectedGlobalCovid19)
+        });
+
+        d3.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv", 
+        function(recoveredGlobalCovid19) {
+            console.log(recoveredGlobalCovid19)
+        });
+    }
+
+
+
+
+
+
+/*
 fetch("src/data/data.json")
     .then(e => e.json())
     .then(json => {
@@ -12,4 +48,6 @@ fetch("src/data/data.json")
         document.body.appendChild(title);
         document.body.appendChild(content);
         document.body.appendChild(image);
-    });
+    })
+    */
+
