7b762da7f2793bb35ab6fa47917c6cddd6c3c173
NadineDon <Nadine.Donie@stud.uni-regensburg.de>
2020-04-16 14:26:47 +0200

    data clean - infected, recovered

--- src/js/index.js
+++ src/js/index.js
@@ -4,26 +4,10 @@ import { Data } from "/src/data/data.js";
 let data = new Data();
 
 //let deathsUncleaned = data.getDeathsGlobalCovid19Data();
-let deaths = data.columDeathsGlobalCovid19Data();
-console.log(deaths);
+let deaths = data.deathsGlobalCovid19Data();
+let infected = data.infectedGlobalCovid19Data();
+let recovered = data.recoveredCoivd19Data();
 
 
 
-/*
-fetch("src/data/data.json")
-    .then(e => e.json())
-    .then(json => {
-        let title = document.createElement("h2"),
-            content = document.createElement("p"),
-            image = document.createElement("img");
-
-        title.innerHTML = json.title;
-        content.innerHTML = json.content;
-        image.setAttribute("src", "./src/images/sql magic.jpg");
-
-        document.body.appendChild(title);
-        document.body.appendChild(content);
-        document.body.appendChild(image);
-    })
-    */
 
